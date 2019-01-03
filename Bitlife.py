ffda56dfa25c2363df1169adc6879231ae3f1935
Thomas Cutro <tcutro@ucvts.org>
2019-01-03 12:24:37 -0500

    Basic Class System

---
+++ Bitlife.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self,name,age,gender,wealth,happiness,health,smarts,looks):
+    self.name = name
+    self.age = age
+    self.gender = gender
+    self.wealth = wealth
+    self.happiness = happiness
+    self.health = health
+    self.smarts = smarts 
+    self.looks = looks 
+  
