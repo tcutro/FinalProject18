7dd13fd9fb37fe4850ce34bf025385e0442d38ef
Thomas Cutro <tcutro@ucvts.org>
2019-01-03 13:23:24 -0500

    random generated stats

--- Bitlife.py
+++ Bitlife.py
@@ -13,16 +13,26 @@ class Person:
     
   def introduce(self):
     print(f"I was born {self.gender}. My name is {self.name}.")
+
+  def stats(self):
+      print(f"Stats: \n Wealth: {self.wealth} \n Happiness: {self.happiness} \n Health: {self.health} \n Smarts: {self.smarts} \n Looks: {self.looks} \n")
   
   def age_up(self):
     self.age += 1 
     print(f'You are now {self.age} years old.')
+    
+happiness = random.randint(1,101)
+health = random.randint(1,101)
+smarts = random.randint(1,101)
+looks = random.randint(1,101)
 
-character = Person(input("What is your name"),"alive",0,"male",0,40,45,56,78)
+
+character = Person(input("What is your name"),"alive",0,"male",0,happiness,health,smarts,looks)
 
 print(character.introduce())
 
 while character.status == "alive":
+    print(character.stats())
     print("\n Moves: \n a = grow older a year \n")
     move = input("What is your next move \n")
     if move == 'a':
