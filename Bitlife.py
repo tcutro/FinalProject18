68f2699fb01d1eb104fca18c37a1288cd87d88df
Thomas Cutro <tcutro@ucvts.org>
2019-01-03 13:09:16 -0500

    Age up command added

--- Bitlife.py
+++ Bitlife.py
@@ -1,7 +1,8 @@
 import random
 class Person:
-  def __init__(self,name,age,gender,wealth,happiness,health,smarts,looks):
+  def __init__(self,name,status,age,gender,wealth,happiness,health,smarts,looks):
     self.name = name
+    self.status = status
     self.age = age
     self.gender = gender
     self.wealth = wealth
@@ -17,9 +18,12 @@ class Person:
     self.age += 1 
     print(f'You are now {self.age} years old.')
 
-character = Person(input("What is your name"),0,"male",0,40,45,56,78)
+character = Person(input("What is your name"),"alive",0,"male",0,40,45,56,78)
 
 print(character.introduce())
 
-
-    
+while character.status == "alive":
+    print("\n Moves: \n a = grow older a year \n")
+    move = input("What is your next move \n")
+    if move == 'a':
+        character.age_up()
