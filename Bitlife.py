0649da221d072fc67852a9cf42ae162b8797c547
Thomas Cutro <tcutro@ucvts.org>
2019-01-03 12:54:54 -0500

    basic generation of character

--- Bitlife.py
+++ Bitlife.py
@@ -1,3 +1,4 @@
+import random
 class Person:
   def __init__(self,name,age,gender,wealth,happiness,health,smarts,looks):
     self.name = name
@@ -7,5 +8,18 @@ class Person:
     self.happiness = happiness
     self.health = health
     self.smarts = smarts 
-    self.looks = looks 
+    self.looks = looks
+    
+  def introduce(self):
+    print(f"I was born {self.gender}. My name is {self.name}.")
   
+  def age_up(self):
+    self.age += 1 
+    print(f'You are now {self.age} years old.')
+
+character = Person(input("What is your name"),0,"male",0,40,45,56,78)
+
+print(character.introduce())
+
+
+    
