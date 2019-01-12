2b8c6de7e37a4bd86a5186f90637586918599ef9
Thomas Cutro <tcutro@ucvts.org>
2019-01-12 12:11:22 -0500

    added two more scenarios

--- Bitlife.py
+++ Bitlife.py
@@ -1,44 +1,4 @@
 import random
-class Person:
-  def __init__(self,name,status,month,grade,gender,wealth,happiness,health,smarts,looks):
-    self.name = name
-    self.status = status
-    self.month = month
-    self.grade= grade
-    self.gender = gender
-    self.wealth = wealth
-    self.happiness = happiness
-    self.health = health
-    self.smarts = smarts 
-    self.looks = looks
-    
-  def introduce(self):
-    print(f"Welcome {self.name} to your first day of 9th grade. You were born {self.gender}.")
-
-  def stats(self):
-      print(f"Grade: {self.grade} \nMonth: {self.month} \n\n Stats:\n Wealth: {self.wealth} \n Happiness: {self.happiness} \n Health: {self.health} \n Smarts: {self.smarts} \n Looks: {self.looks} \n")
-  
-  def month_up(self):
-    self.month += 1 
-    print(f'It is the next month')
-
-  def school_scenarios(self):
-    if self.grade == 9:
-      scenario = random.randint(1,12)
-      if scenario == 1:
-        x = input("A bully wants to take your lunch. \n What will you do? \n \n a= punch the bully \n b= give the bully your lunch \n c = run away \n")
-        if x == 'a':
-          fight = random.randint(1,2)
-          if fight == 1:
-            print("You got beat up")
-            self.health -= 10
-          if fight == 2:
-            print("You won")
-            self.happiness += 10
-        if x == 'b':
-          self.happiness -= 10
-        if x == 'c':
-          self.smarts += 5
     
 happiness = random.randint(1,101)
 health = random.randint(1,101)
@@ -48,6 +8,72 @@ death_chance= 125
 possible_genders = ["male","female", "alien"]
 gender= random.choice(possible_genders)
 
+class Person:
+  def __init__(self,name,status,month,grade,gender,popularity,happiness,health,smarts,looks):
+    self.name = name
+    self.status = status
+    self.month = month
+    self.grade= grade
+    self.gender = gender
+    self.popularity = popularity
+    self.happiness = happiness
+    self.health = health
+    self.smarts = smarts 
+    self.looks = looks
+    
+  def introduce(self):
+    print(f"Welcome {self.name} to your first day of 9th grade. You were born {self.gender}.")
+
+  def stats(self):
+      print(f"Grade: {self.grade} \nMonth: {self.month} \n\n Stats:\n Popularity: {self.popularity} \n Happiness: {self.happiness} \n Health: {self.health} \n Smarts: {self.smarts} \n Looks: {self.looks} \n")
+  
+  def month_up(self):
+    self.month += 1 
+    print(f'It is the next month')
+
+  def school_scenarios(self):
+    completed_scenarios = []
+    if self.grade == 9:
+      scenario = random.randint(1,3)
+      if scenario == 1:
+        if "1" not in completed_scenarios:
+          completed_scenarios += "1"
+          x = input("A bully wants to take your lunch. \n What will you do? \n \n a= punch the bully \n b= give the bully your lunch \n c = run away \n")
+          if x == 'a':
+            fight = random.randint(1,3)
+            if fight == 1:
+              print("You got beat up")
+              self.health -= 10
+            if fight == 2:
+              print("You won")
+              self.happiness += 10
+          elif x == 'b':
+            self.happiness -= 10
+          elif x == 'c':
+            self.smarts += 5
+      elif scenario == 2:
+        if "2" not in completed_scenarios:
+          completed_scenarios += "2"
+          x = input("You are assigned a group project from your freshman technology teacher. What are you going to do? \n \n a= do all the work \n b= do none of the work \n c= split up the work evenly")
+          if x == 'a':
+            self.smarts += 10
+          elif x == 'b':
+            self.popularity -= 10
+          elif x == 'c':
+            self.popularity += 5
+      elif scenario == 3:
+        if "3" not in completed_scenarios:
+          completed_scenarios += "3"
+          x = input("Your mom is yelling at you for getting a bad grade in Dr. Gupta's freshman biology class. What is your next move? \n \n a= give up and cry \n b= study harder \n c= bribe the teacher")
+          if x == 'a':
+            self.happiness -= 10
+          elif x == 'b':
+            self.smarts += 10
+          elif x == 'c':
+            self.smarts -= 10
+          
+        
+        
 character = Person(input("What is your name"),"alive",0,9,gender,0,happiness,health,smarts,looks)
 
 character.introduce()
@@ -74,11 +100,3 @@ while character.status == "alive":
   if move == 'a':
     print("\n \n \n \n \n \n \n \n \n \n \n")
     character.month_up()
-
-
-    
-
-
-      
-    
-        
